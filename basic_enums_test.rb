ac1f49e66f1201a24e19aa1effb603dae56a7569
Katrina Owen <katrina.owen@gmail.com>
2013-09-20 20:19:20 -0600

    Move harder questions to bonus section

--- basic_enums_test.rb
+++ basic_enums_test.rb
@@ -19,20 +19,5 @@ class BasicEnumsTest < Minitest::Test
     assert_equal 15, sum
   end
 
-  def test_max_value
-    skip
-    max_num = 0
-    numbers = [2, 16, 6, 50, 12]
-    # write code here
-    assert_equal 50, max_num
-  end
-  
-  def test_simple_factorial
-    skip
-    sum_of_factorials = 0
-    numbers = [1, 2, 3, 4, 5]
-    # write code here
-    assert_equal 153, sum_of_factorials
-  end
 end
 
---
+++ bonus_questions_test.rb
@@ -0,0 +1,34 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class BonusQuestionsTest < Minitest::Test
+  def test_7_factorial
+    # Example: "Five factorial", written 5!, is
+    # 1 * 2 * 3 * 4 * 5
+    product = 1
+    numbers = [1, 2, 3, 4, 5, 6, 7]
+    numbers.each do |number|
+      # write code here
+    end
+    assert_equal 5040, product
+  end
+
+  def test_sum_of_factorials
+    skip
+    sum_of_factorials = 0
+    numbers = [1, 2, 3, 4, 5]
+    # write code here
+    assert_equal 153, sum_of_factorials
+  end
+
+  def test_max_value
+    skip
+    max_num = 0
+    numbers = [2, 16, 6, 50, 12]
+    # write code here
+    assert_equal 50, max_num
+  end
+
+end
+

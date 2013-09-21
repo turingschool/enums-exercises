e1fd877eabf9195670f19f9c8ac7fcfde5953849
Katrina Owen <katrina.owen@gmail.com>
2013-09-21 08:39:19 -0600

    Improve #all?test suite
    * Add a passing test showing the #each pattern
    * Add a passing test showing the #all? pattern
    * Add comments to help guide the process
    * Normalize syntax to minimize confusion

--- are_they_all_test.rb
+++ are_they_all_test.rb
@@ -3,29 +3,37 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class AreTheyAllTest < Minitest::Test
-
+  # You get the first test for free... it's already passing.
   def test_not_all_zeros
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
-      # write code here
+      all_zeros = false unless number.zero?
     end
     refute all_zeros
   end
 
+  # This test is missing a single line of code
   def test_all_zeros
     skip
     numbers = [0, 0, 0, 0, 0, 0, 0, 0]
-    # write code here
+    all_zeros = true
+    numbers.each do |number|
+      # write code here
+    end
     assert all_zeros
   end
 
+  # This test is missing the whole loop
   def test_all_gone
     skip
     words = %w(gone gone gone gone gone gone gone)
+    all_gone = true
     # write code here
     assert all_gone
   end
+
+  # From here on out, you're pretty much on your own...
 
   def test_not_all_gone
     skip
@@ -61,6 +69,15 @@ class AreTheyAllTest < Minitest::Test
     # write code here
     assert all_false
   end
+end
 
+class AreTheyAllUsingAllTest < Minitest::Test
+  def test_not_all_zeros_using_all
+    numbers = [0, 0, 0, 0, 1, 0, 0, 0]
+    all_zeros = numbers.all? do |number|
+      number.zero?
+    end
+    refute all_zeros
+  end
 end
 

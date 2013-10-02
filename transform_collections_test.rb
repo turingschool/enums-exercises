b5183dab1a0a7bb7e25bcccc3bd8350bda4cc491
seeflanigan <seeflanigan@gmail.com>
2013-10-02 13:32:18 -0600

    Revert "Add some exercises for #group_by"
    This reverts commit 07b24c98d776b879d2dcf6a6e5a9386075fa25ef.

--- transform_collections_test.rb
+++ transform_collections_test.rb
@@ -15,7 +15,6 @@ class TransformCollectionsTest < MiniTest::Unit::TestCase
 
   # This test is missing a single line of code
   def test_doubles
-    skip
     numbers = [1, 2, 3, 4, 5]
     doubles = []
     numbers.each do |number|
@@ -88,39 +87,3 @@ class TransformCollectionsWithMapTest < MiniTest::Unit::TestCase
   end
 end
 
-class TransformCollectionsUsingGroupByTest < MiniTest::Unit::TestCase
-  def test_group_by_length
-    words = ["sue", "alice", "steve", "sally", "adam", "fort", "tops", "dog", "cat"]
-    words_by_length = words.group_by do |word|
-      word.length
-    end
-    assert_equal({3 => ["sue", "dog", "cat"],
-                  4 => ["adam", "fort", "tops"],
-                  5 => ["alice", "steve", "sally"]}, words_by_length)
-  end
-
-  def test_group_even_and_odd
-    skip
-    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-    # write code here
-    assert_equal({1=>[1, 1, 3, 5, 13, 21, 55], 0=>[2, 8, 34]}, odd_and_even)
-  end
-
-  def test_group_by_first_letter
-    skip
-    words = ["ant", "axis", "albatross", "bolt", "badge", "butter", "car", "cdr", "column"]
-    # write code here
-    assert_equal({"a"=>["ant", "axis", "albatross"],
-                  "b"=>["bolt", "badge", "butter"],
-                  "c"=>["car", "cdr", "column"]}, words_by_first_letter)
-  end
-
-  def test_group_by_frequency
-    skip
-    words = ["one", "two", "three", "two", "three", "three"]
-    # write code here
-    assert_equal({"one"=>["one"],
-                  "two"=>["two", "two"],
-                  "three"=>["three", "three", "three"]}, words_by_frequency)
-  end
-end

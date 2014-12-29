1ea21b7d063645feb96743f33379fbd7830d0c48
Paul Grever <paulgrever@gmail.com>
2014-12-29 13:48:03 -0700

    added my code for maps

--- exercises/map_pattern_test.rb
+++ exercises/map_pattern_test.rb
@@ -17,51 +17,73 @@ class MapPatternTest < Minitest::Test
     numbers = [1, 2, 3, 4, 5]
     doubles = []
     numbers.each do |number|
-      # Your code goes here
+      doubles.push(number * 2)
     end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
   def test_squares
-    skip
+    
     numbers = [1, 2, 3, 4, 5]
     squares = []
-    # Your code goes here
+    numbers.each do |number|
+      squares << number ** 2
+    end
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
   def test_lengths
-    skip
+    
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    lengths = []
+    names.each do |name|
+      lengths << name.length 
+    end
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
   def test_normalize_zip_codes
-    skip
+    
     numbers = [234, 10, 9119, 38881]
-    # Your code goes here
+    zip_codes = []
+    numbers.each do |number|
+      zip_codes << number.to_s.rjust(5, '0')
+    end
     assert_equal ["00234", "00010", "09119", "38881"], zip_codes
   end
 
   def test_backwards
-    skip
+    
     names = ["alice", "bob", "charlie", "david", "eve"]
     # Your code goes here
+    backwards = Array.new
+    names.each do |n|
+      backwards.push(n.reverse)
+    end
+
     assert_equal ["ecila", "bob", "eilrahc", "divad", "eve"], backwards
   end
 
   def test_words_with_no_vowels
-    skip
+    
     words = ["green", "sheep", "travel", "least", "boat"]
     # Your code goes here
+    without_vowels = []
+    words.each do |n|
+      without_vowels << n.gsub(/[aeiou]/, "")
+    end
     assert_equal ["grn", "shp", "trvl", "lst", "bt"], without_vowels
   end
 
   def test_trim_last_letter
-    skip
+    
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
     # Your code goes here
+    trimmed = []
+    animals.each do |n|
+      trimmed.push(n.chop)
+      
+    end
     assert_equal ["do", "ca", "mous", "fro", "platypu"], trimmed
   end
 
--- exercises/map_test.rb
+++ exercises/map_test.rb
@@ -15,50 +15,58 @@ class MapTest < Minitest::Test
   def test_doubles
     numbers = [1, 2, 3, 4, 5]
     doubles = numbers.map do |number|
-      # Your code goes here
+      number * 2
     end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
   def test_squares
-    skip
+    
     numbers = [1, 2, 3, 4, 5]
-    # Your code goes here
+    squares = numbers.map do |number|
+      number ** 2
+    end
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
   def test_lengths
-    skip
+    
     names = ["alice", "bob", "charlie", "david", "eve"]
     # Your code goes here
+    lengths = names.map do |name|
+      name.length
+    end
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
   def test_normalize_zip_codes
-    skip
+    
     numbers = [234, 10, 9119, 38881]
-    # Your code goes here
+    zip_codes = numbers.map {|number| number.to_s.rjust(5, '0')}
     assert_equal ["00234", "00010", "09119", "38881"], zip_codes
   end
 
   def test_backwards
-    skip
+    
     names = ["alice", "bob", "charlie", "david", "eve"]
     # Your code goes here
+    backwards = names.map {|n| n.reverse}
     assert_equal ["ecila", "bob", "eilrahc", "divad", "eve"], backwards
   end
 
   def test_words_with_no_vowels
-    skip
+    
     words = ["green", "sheep", "travel", "least", "boat"]
     # Your code goes here
+    without_vowels = words.map {|n| n.gsub(/[aeiou]/, '')}
     assert_equal ["grn", "shp", "trvl", "lst", "bt"], without_vowels
   end
 
   def test_trim_last_letter
-    skip
+    
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
     # Your code goes here
+    trimmed = animals.map {|n| n.chop}
     assert_equal ["do", "ca", "mous", "fro", "platypu"], trimmed
   end
 

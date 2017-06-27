6104313818884c2de077e5bfa184bc5e28b04d30
Chantal Justamond <cjustamm66@me.com>
2017-06-27 10:18:56 -0600

    find and sort_by done! :)

--- exercises/test/sort_by_test.rb
+++ exercises/test/sort_by_test.rb
@@ -2,67 +2,57 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class SortByPatternTest < Minitest::Test
+class SortByTest < Minitest::Test
 
   def test_sort_alphabetically
     words = ["broccoli", "Carrots", "FISH", "Bacon", "candy"]
-    transformed = []
-    words.each do |word|
-      transformed << [word.downcase, word]
-    end
-    transformed = transformed.sort
-    sorted = []
-    transformed.each do |sort_key, word|
-      sorted << word
+    sorted = words.sort_by do |word|
+      word.downcase
     end
     assert_equal ["Bacon", "broccoli", "candy", "Carrots", "FISH"], sorted
   end
 
   def test_sort_alphabetically_by_last_letter
     things = ["pill", "box", "glass", "water", "sponge"]
-    transformed = []
-    things.each do |thing|
-      # Your code goes here
-    end
-    transformed = transformed.sort
-    sorted = []
-    transformed.each do |sort_key, thing|
-      sorted << thing
+    sorted = things.sort_by do |thing|
+      thing[-1]
     end
     assert_equal ["sponge", "pill", "water", "glass", "box"], sorted
   end
 
   def test_sort_by_distance
-    skip
+
     distances = ["1cm", "9cm", "30cm", "4cm", "2cm"]
-    transformed = []
-    # Your code goes here
-    transformed = transformed.sort
-    sorted = []
-    transformed.each do |sort_key, distance|
-      sorted << distance
+    sorted = distances.sort_by do |distance|
+      distance.to_i
     end
     assert_equal ["1cm", "2cm", "4cm", "9cm", "30cm"], sorted
   end
 
   def test_sort_by_length
-    skip
+
     words = ["heteromorph", "ancyloceratina", "bioengineering", "mathematical", "bug"]
-    # Your code goes here
+    sorted = words.sort_by do |word|
+      word.length
+    end
     assert_equal ["bug", "heteromorph", "mathematical", "ancyloceratina", "bioengineering"], sorted
   end
 
   def test_sort_by_proximity_to_ten
-    skip
+
     prices = [3.02, 9.91, 17.9, 10.01, 11.0]
-    # Your code goes here
+    sorted = prices.sort_by do |price|
+      (10 - price).abs
+    end
     assert_equal [10.01, 9.91, 11.0, 3.02, 17.9], sorted
   end
 
   def test_sort_by_number_of_cents
-    skip
+
     prices = [3.02, 9.91, 7.9, 10.01, 11.0]
-    # Your code goes here
+    sorted = prices.sort_by do |price|
+      price - price.floor
+    end
     assert_equal [11.0, 10.01, 3.02, 7.9, 9.91], sorted
   end
 
